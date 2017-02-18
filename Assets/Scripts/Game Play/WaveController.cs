0d0a2bd024eabab4f1982b14a7033c7b27f7194e
Ohad Ben-Nun <bnohad@gmail.com>
2017-02-18 18:13:31 +0200

    Created Wave Controller template

---
+++ Assets/Scripts/Game Play/WaveController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+    private static WaveController _instance;
+    private int _wave;
+
+    public static WaveController Instance { get; private set; }
+    public int Wave { get; set; }
+    
+    public WaveController()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void Init()
+    {
+        Wave = 1;
+    }
+
+    public void InitNextWave(int numOfEnemies, int extraAmmo, float gameSpeed, int gameDiffuclty)
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
